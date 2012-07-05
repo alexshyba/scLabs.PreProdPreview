5dd7ab34c8cc6aeed3a31056b87c73ad047293a0
Alex Shyba <alex.shyba@gmail.com>
2012-07-05 11:52:52 -0700

    First commit

---
+++ Config.cs
@@ -0,0 +1,46 @@
+﻿namespace scLabs.PreProdPreview
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using Sitecore;
+   using Sitecore.Configuration;
+   using Sitecore.Data;
+   using Sitecore.Sites;
+
+   public static class Config
+   {
+      public static string FilteredWorkflowStates
+      {
+         get
+         {
+            return Settings.GetSetting("PreProdPreview.FilteredWorkflowStates");
+         }
+      }
+
+      public static IEnumerable<ID> WorkflowStatesToFilter
+      {
+         get
+         {
+            var stateIds = MainUtil.RemoveEmptyStrings(FilteredWorkflowStates.ToLower().Split(new[] { '|' }, StringSplitOptions.RemoveEmptyEntries));
+            return from stateId in stateIds where ID.IsID(stateId) select ID.Parse(stateId);
+         }
+      }
+
+      public static SiteContext NoFilteringSite
+      {
+         get { return SiteContextFactory.GetSiteContext("nofiltering_reserved"); }
+      }
+
+      public static bool FilterByWorkflowEnabled
+      {
+         get
+         {
+            return Context.Site != null &&
+                   Context.Site.SiteInfo.Properties["filterByWorkflow"] != null &&
+                   Context.Site.SiteInfo.Properties["filterByWorkflow"].Equals("true", StringComparison.InvariantCultureIgnoreCase);
+         }
+      }
+   }
+
+}
---
+++ Pipelines/FilterItems/FilterByWorkflowState.cs
@@ -0,0 +1,62 @@
+﻿namespace scLabs.PreProdPreview.Pipelines.FilterItems
+{
+   using System.Linq;
+   using Sitecore;
+   using Sitecore.Data;
+   using Sitecore.Data.Items;
+   using Sitecore.Pipelines.FilterItem;
+   using Sitecore.SecurityModel;
+   using Sitecore.Sites;
+   using Sitecore.StringExtensions;
+
+   public class FilterByWorkflowState
+   {
+      public void Process(FilterItemPipelineArgs args)
+      {
+         var site = Context.Site;
+         if (site == null) return;
+
+         using (new SecurityDisabler())
+         {
+            if (!site.EnableWorkflow && Config.FilterByWorkflowEnabled)
+            {
+               var filtered = ProcessWorkflowStateFiltering(args.FilteredItem);
+               args.FilteredItem = filtered;
+            }
+         }
+      }
+
+      protected virtual Item ProcessWorkflowStateFiltering(Item item)
+      {
+         if (Config.NoFilteringSite == null) return item;
+
+         using (new SiteContextSwitcher(Config.NoFilteringSite))
+         {
+            var versions = item.Versions.GetVersions();
+            if (versions.Length == 0) return item;
+
+            for (var i = versions.Length - 1; i >= 0; i--)
+            {
+               var version = versions[i];
+               if (!ShouldBeFiltered(version))
+               {
+                  return version;
+               }
+            }
+            return null;
+         }
+      }
+
+      protected virtual bool ShouldBeFiltered(Item item)
+      {
+         var currentStateId = item[FieldIDs.WorkflowState];
+
+         if (currentStateId.IsNullOrEmpty()) return false;
+
+         return ID.IsID(currentStateId) &&
+                Config.WorkflowStatesToFilter.Contains(ID.Parse(currentStateId));
+      }
+   }
+}
+
+
